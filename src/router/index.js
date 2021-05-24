d1873d5594fdc966459a06327e6301218e68f6b7
zlgkll <948486501@qq.com>
2021-05-24 10:59:07 +0800

    '初始化项目'

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,45 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../views/Login.vue'
-import Home from '../views/Home.vue'
-import Welcome from '../components/home/Welcome'
-import Users from '../components/home/users/Users'
+
+const Login = () => import('../components/Login')
+const Home = () => import('../components/Home')
+const Welcome = () => import('../components/Welcome')
+
+// import Users from './components/user/Users.vue'
+const Users = () => import( '../components/user/Users.vue')
+// import Rights from './components/power/Rights.vue'
+const Rights = () => import(/* webpackChunkName: "Users_Rights_Roles" */ '../components/power/Rights.vue')
+// import Roles from './components/power/Roles.vue'
+const Roles = () => import(/* webpackChunkName: "Users_Rights_Roles" */ '../components/power/Roles.vue')
+
+// import Cate from './components/goods/Cate.vue'
+const Cate = () => import(/* webpackChunkName: "Cate_Params" */ '../components/goods/Cate.vue')
+// import Params from './components/goods/Params.vue'
+const Params = () => import(/* webpackChunkName: "Cate_Params" */ '../components/goods/Params.vue')
+
+// import Order from './components/order/Order.vue'
+const Order = () => import(/* webpackChunkName: "Order_Report" */ '../components/order/Order.vue')
+
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    redirect: '/login'
-  },
-  {
-    path: '/login',
-    component: Login
-  },
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
   {
     path: '/home',
     component: Home,
     redirect: '/welcome',
     children: [
-      {
-        path: '/welcome',
-        component: Welcome
-      },
-      {
-        path: '/users',
-        component: Users
-      }
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: Users },
+      { path: '/rights', component: Rights },
+      { path: '/roles', component: Roles },
+      { path: '/categories', component: Cate },
+      { path: '/params', component: Params },
+      { path: '/goods', component: GoodsList },
+      { path: '/goods/add', component: Add },
+      { path: '/orders', component: Order },
     ]
   }
 ]
@@ -37,6 +48,11 @@ const router = new VueRouter({
   routes,
   mode: 'history'
 })
+
+const VueRouterPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push (to) {
+  return VueRouterPush.call(this, to).catch(err => err)
+}
 
 // 挂载路由 导航守卫
 // 登录页都可以访问，主页只有携带token信息才能访问，否则转到登录页
